3c4ae499f792cfd00bb1be94366f7b270ae9cbd4
Mochammad Alfin Faturochman <82384511+alfinfaturochmanm@users.noreply.github.com>
2021-12-22 14:57:57 +0700

    progress

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,15 +1,19 @@
 module.exports = (app) => {
 
     const bodyParser = require('body-parser')
+    const expressValidator = require('express-validator')
 
     var UsersControllers = require('../controllers/User')(app);
+    var LocationControllers = require('../controllers/location')(app);
+    // var UserMiddlewares = require('../middlewares/user')(app);
     // var Auth = require('../middleware/Auth')
     // Auth = Auth();
 
     app.use(bodyParser.json())
     app.use(bodyParser.urlencoded({ extended: false }))
+    app.use(expressValidator())
     
-    app.get("/", UsersControllers.addUser);
+    app.get("/location", LocationControllers.getList);
     app.post("/users", UsersControllers.addUser);
     app.delete("/users", UsersControllers.deleteUser);
 }
